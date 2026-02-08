2520779d12f75f25f2a6330187a9716fa280ed38
Aditya Kumar singh <ksingh10adi@gmail.com>
2026-02-08 12:53:14 +0530

    updated files

--- script.js
+++ script.js
@@ -1,40 +1,45 @@
 let inputSlider = document.getElementById("inputSlider");
-let sliderVale = document.getElementById("sliderValue");
-let passBox = document.getElementById('passBox');
+let sliderValue = document.getElementById("SliderValue");
+let passBox = document.getElementById("passBox");
 
-let lowercase = document.getElementById('lowercase');
-let uppercase = document.getElementById('uppercase');
-let numbers = document.getElementById('numbers');
-let symbols = document.getElementById('symbols');
-let genBtn = document.getElementById('genBtn');
+let lowercase = document.getElementById("lowercase");
+let uppercase = document.getElementById("uppercase");
+let numbers = document.getElementById("numbers");
+let symbols = document.getElementById("symbols");
+let genBtn = document.getElementById("getBtn");
 
-sliderVale.textContent = inputSlider.value;
-inputSlider.addEventListener('input', () => {
-    sliderVale.textContent = inputSlider.value;
-})
+sliderValue.textContent = inputSlider.value;
 
-genBtn.addEventListener('click', () => {
-    passBox.value = generatepassword();
-})
+inputSlider.addEventListener("input", () => {
+    sliderValue.textContent = inputSlider.value;
+});
+
+genBtn.addEventListener("click", () => {
+    passBox.value = generatePassword();
+});
 
 let upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let lowerChars = "abcdefghijklmnopqrstuvwxyz";
 let allnumbers = "0123456789";
 let allsymbols = "~!@#$%^&*";
 
-function generatepassword() {
+function generatePassword() {
     let genPassword = "";
     let allChars = "";
 
     allChars += lowercase.checked ? lowerChars : "";
     allChars += uppercase.checked ? upperChars : "";
-    allChars += numbers.checked ? numbers : "";
+    allChars += numbers.checked ? allnumbers : "";
     allChars += symbols.checked ? allsymbols : "";
 
-    let i = 1;
-    while(i <= inputSlider.value){
-        genPassword += allChars.charAt(Math.floor(Math.random() * upperChars.length));
-        i++;
+    if (allChars.length === 0) {
+        return "Select options";
+    }
+
+    for (let i = 0; i < inputSlider.value; i++) {
+        genPassword += allChars.charAt(
+            Math.floor(Math.random() * allChars.length)
+        );
     }
 
     return genPassword;
